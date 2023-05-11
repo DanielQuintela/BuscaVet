01082c2ff0d10a700eab3335052fa7fd594a9a2e
Daniel <53890254+Danieldev-sweet@users.noreply.github.com>
2023-05-11 08:26:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
-st.set_page_config(page_title='BuscaVet', page_icon=':mag:')
 import pandas as pd
 import sqlite3
 import Banco.banco_dados as Banco
-import Page.cadastro as PageCadastro , Page.usuario as PageUsuario, Page.veterinario as PageVeterinario, Page.adm as PageAdm
+import Page.cadastro as PageCadastro , Page.usuario as PageUsuario, Page.veterinario as PageVeterinario, Page.adm as PageAdm, Page.login as PageLogin
 from sqlite3 import Error
 
 
@@ -31,86 +30,70 @@ def aprovar():
 def fechar():
     st.session_state.login = False
 
+start_bar = st.sidebar.empty()
+checkbox_placeholder = st.sidebar.empty()
+titulo = st.empty()
+texto = st.empty()
+#Abaixo o contúdo da página incial
 
-start = st.sidebar.empty()
+title = titulo.title('Tela principal')
+text = texto.text('Em construção 🏗')
 
-paginaSelecionada = start.selectbox('Selecione o caminho',
-                                         ['Tela de inicio', 'Login e/ou Cadastro'], key='selectbox3')
 
-if paginaSelecionada == 'Tela de inicio':
-    st.title('Tela principal')
-    st.text('Em construção 🏗')
-   
+login, cadastro = start_bar.tabs(["Login", "Cadastro"])
+
+with cadastro:
+    PageCadastro.Cadastro()
     
+with login:
+    retorno = PageLogin.login()
+    nome = retorno[0]
+    senha = retorno[1]
+    situacao = retorno[2]
+    marcado = checkbox_placeholder.button('Login')
+
+
+    if marcado:
+        aprovar()
+        
+
+if st.session_state.login:    
+    # if input_senha_func == '1234':
+    Banco.create_usertable()
+    Banco.create_veterinario()
+    Banco.criar_clinica()
+    user = Banco.login_user(nome, senha)
+    vet = Banco.login_veterinario(nome, senha, situacao)
+    #clinica = Banco.login_clinica(nome, senha, situacao)
     
-    
-elif paginaSelecionada == 'Login e/ou Cadastro':
-    title = st.empty()
-    titulo = title.title("Seja Bem vindo a Tela de Cadastro !")
-    selectbox_placeholder = st.sidebar.empty()
-    nome_place = st.empty()
-    senha_place = st.empty()
-    checkbox_placeholder = st.empty()
-    escolha = selectbox_placeholder.selectbox('Selecione a Opção', ['Login', 'Cadastro'])
 
-    if escolha == 'Cadastro':
-        PageCadastro.Cadastro()
-   
-                        
+    if user:
+        start_bar.empty()
+        checkbox_placeholder.empty()
+        titulo.empty()
+        texto.empty()
 
-    if escolha == 'Login':
-        titulo = title.title("Bem vindo de Volta !")
-        nome = nome_place.text_input('Insira seu E-mail')
-        senha = senha_place.text_input('Insira a senha', type='password')
-        situacao = 'Aprovado'
-        marcado = checkbox_placeholder.button('Login')
+        Page = PageUsuario.Usuario(nome)
 
-        if marcado:
-            aprovar()
+        
+    elif vet:
+        
+        start_bar.empty()
+        checkbox_placeholder.empty()
+        titulo.empty()
+        texto.empty()
 
-        if st.session_state.login:    
-            # if input_senha_func == '1234':
-            Banco.create_usertable()
-            Banco.create_veterinario()
-            Banco.criar_clinica()
-            user = Banco.login_user(nome, senha)
-            vet = Banco.login_veterinario(nome, senha, situacao)
-            #clinica = Banco.login_clinica(nome, senha, situacao)
-            
-
-            if user:
-                start.empty()
-                title.empty()
-                selectbox_placeholder.empty()
-                nome_place.empty()
-                senha_place.empty()
-                checkbox_placeholder.empty()
-
-                Page = PageUsuario.Usuario(nome)
-
-               
-            elif vet:
-                start.empty()
-                title.empty()
-                selectbox_placeholder.empty()
-                nome_place.empty()
-                senha_place.empty()
-                checkbox_placeholder.empty()
-
-                PageVeterinario.Veterinario(nome)
+        PageVeterinario.Veterinario(nome)
 
 
-            elif senha == '0809':
-                start.empty()
-                title.empty()
-                
-                nome_place.empty()
-                senha_place.empty()
-                checkbox_placeholder.empty()
-                PageAdm.Adm()
-                            
-                
-            else:
-                st.warning("Usuário incorreto ou Inexistente")
-    
-   
+    elif senha == '123':
+        titulo.empty()
+        texto.empty()
+        checkbox_placeholder.empty()
+        PageAdm.Adm()
+                    
+        
+    else:
+        st.warning("Usuário incorreto ou Inexistente")
+
+       
