a6d0afc56bf2770a76c9ae9e2c74a63fe80fc208
Daniel <53890254+Danieldev-sweet@users.noreply.github.com>
2023-06-12 10:17:39 -0300

    Update banco_dados.py

--- Banco/banco_dados.py
+++ Banco/banco_dados.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import sqlite3
 import threading
+from datetime import datetime
 conn_local = threading.local()
 
 
@@ -62,30 +63,36 @@ def create_pet():
     con = sqlite3.connect('banco_programa.db')
     cursor = con.cursor()
     cursor.execute(
-        'CREATE TABLE IF NOT EXISTS pet(dono TEXT, nome TEXT, raca TEXT,cor TEXT, idade NUMERIC)')
+        'CREATE TABLE IF NOT EXISTS pet(email TEXT, dono TEXT, nome TEXT, raca TEXT,cor TEXT, data_nascimento DATE)')
     cursor.close()
     con.close()
 
-def add_pet(dono, nome, raca, cor, idade):
+def add_pet(email, dono, nome, raca, cor, data_nascimento):
     con = sqlite3.connect('banco_programa.db')
     cursor = con.cursor()
     cursor.execute(
-        'INSTERT INTO pet(dono, nome, raca, cor, idade) VALUES (?,?,?,?,?)', (dono, nome, raca, cor, idade))
+        'INSERT INTO pet(email, dono, nome, raca, cor, data_nascimento) VALUES (?,?,?,?,?,?)', (email, dono, nome, raca, cor, data_nascimento))
     con.commit()
     cursor.close()
     con.close()
 
 
-def consulta_pet(dono):
+def consulta_pet(email):
     con = sqlite3.connect('banco_programa.db')
     cursor = con.cursor()
-    cursor.execute(
-        'SELECT * FROM pet WHERE dono = ? ', (dono)
-    )
+    cursor.execute('SELECT dono, nome , raca, cor, data_nascimento FROM pet WHERE email = ?', (email,))
     data = cursor.fetchall()
     cursor.close()
     con.close()
     return data
+
+def calculate_age(date_of_birth):
+    # Calcula a idade com base na data de nascimento
+    today = datetime.today().date()
+    age = today.year - date_of_birth.year
+    if today.month < date_of_birth.month or (today.month == date_of_birth.month and today.day < date_of_birth.day):
+        age -= 1
+    return age
 
 #criar no banco o veterinário
 
@@ -114,10 +121,72 @@ def especialidade(email, especialidade, localidade):
     cursor.close()
     con.close()
 
-def busca_especialidade(email):
+def busca_especialidade_nome(email):
     con = sqlite3.connect('banco_programa.db')
     cursor = con.cursor()
-    cursor.execute('SELECT especialidade FROM veterinario WHERE email = ?', (email,))
+    cursor.execute('SELECT especialidade, localidade FROM veterinario WHERE email = ?', (email,))
+    data = cursor.fetchone()
+    cursor.close()
+    con.close()
+    if data:
+        data = data[0:]
+    return data
+
+def busca_especialidade_geral():
+    con = sqlite3.connect('banco_programa.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT nome, especialidade FROM veterinario')
+    data = cursor.fetchall()
+    cursor.close()
+    con.close()
+
+    especialidades = set()
+    for row in data:
+        especialidades.add(row[1])
+
+    return list(especialidades)
+
+ 
+
+def busca_medicos_por_especialidade(especialidade):
+    con = sqlite3.connect('banco_programa.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT nome, especialidade, localidade, telefone FROM veterinario WHERE especialidade = ?', (especialidade,))
+    data = cursor.fetchall()
+    cursor.close()
+    con.close()
+
+    # medicos = [row[0] for row in data]
+    return data
+
+def busca_especidalidade_por_local(localidade):
+    con = sqlite3.connect('banco_programa.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT nome, especialidade, localidade FROM veterinario WHERE localidade = ?', (localidade,))
+    data = cursor.fetchall()
+    cursor.close()
+    con.close()
+
+def localidade_geral():
+    con = sqlite3.connect('banco_programa.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT localidade FROM veterinario')
+    data = cursor.fetchall()
+    cursor.close()
+    con.close()
+
+    localidade = set()
+    for row in data:
+        localidade.add(row[0])
+
+    return list(localidade)
+
+
+
+def busca_localidade_nome(nome):
+    con = sqlite3.connect('banco_programa.db')
+    cursor = con.cursor()
+    cursor.execute('SELECT localidade FROM veterinario WHERE nome = ?', (nome,))
     data = cursor.fetchone()
     cursor.close()
     con.close()
